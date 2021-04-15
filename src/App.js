a4e0f93e71eb16834061b44f015d9d037fb18531
Ayazhan <ayazhan.shansharova@mail.ru>
2021-04-15 20:09:30 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Searchpage from './comp/Searchpage';
+
+function App() {
+  return (
+    <div className="App">
+      <Searchpage/>
+    </div>
+  );
+}
+
+export default App;
